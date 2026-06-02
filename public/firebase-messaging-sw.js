ab832a1c5e71a052d7b88885b989f5f9197e8136
jungjin20020506 <132337503+jungjin20020506@users.noreply.github.com>
2026-06-02 09:22:36 +0900

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   projectId: "project-104956788310687609",
   storageBucket: "project-104956788310687609.firebasestorage.app",
   messagingSenderId: "384562806148",
-  appId: "1:384956788310687609:web:d8bfb28928c13e671d1"
+  appId: "1:384562806148:web:d8bfb83b28928c13e671d1"
 };
 
 // 3. 앱 초기화 및 메시징 객체 생성
